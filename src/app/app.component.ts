8df0cb5fe001479acf68f129c07cb73238d457f8
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-01-21 11:14:47 -0300

    feat: add items to sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PageLayoutComponent } from "./components/layout/page-layout/page-layout.component";
+import { SidebarSections } from './models/navbar-items.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,48 @@ import { PageLayoutComponent } from "./components/layout/page-layout/page-layout
 })
 export class AppComponent {
   title = 'wallet-manager';
+  sidebarSections: SidebarSections[] = [
+    {
+    label: 'Wallet',
+    collapsed: false,
+    items: [
+      {
+        name: 'Dashboard',
+        icon: 'fi-sr-dashboard',
+        route: '/dashboard'
+      },
+      {
+        name: 'Smart Contracts',
+        icon: 'fi-sr-document',
+        route: '/smart-contract'
+      },
+      {
+        name: 'NFTs View',
+        icon: 'fi-sr-floor-layer',
+        route: '/nfts'
+      },
+      {
+        name: 'Logs',
+        icon: 'fi-sr-receipt',
+        route: '/logs'
+      },
+    ],
+  },
+  {
+    label: 'Trends',
+    collapsed: false,
+    items: [
+      {
+        name: 'Market Overview',
+        icon: 'fi-sr-chart-simple',
+        route: '/overview'
+      },
+      {
+        name: 'Social Trends',
+        icon: 'fi-sr-thumbs-up',
+        route: '/trends'
+      },
+    ],
+  },
+  ]
 }
