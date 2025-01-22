6ac756c91fc2fc76acaaabe536071331c978a84e
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-01-21 23:31:29 -0300

    feat: create initial structure of theme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsla(var(--primary))",
+        background: "hsla(var(--background))",
+        foreground: "hsla(var(--foreground))",
+        accentForeground: "hsla(var(--accent-foreground))",
+        accent: "hsla(var(--accent))",
+        muted: "hsla(var(--muted))",
+        mutedForeground: "hsla(var(--muted-foreground))",
+        card: "hsla(var(--card))",
+        border: "hsla(var(--border))",
+      }
+    },
   },
   plugins: [],
 }
