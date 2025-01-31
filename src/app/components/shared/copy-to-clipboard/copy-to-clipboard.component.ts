4da546f7cd68194e046523052c3916700be834c5
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-01-31 15:49:35 -0300

    feat(copy-to-clipboard): implement custom tooltip status on copy

--- src/app/components/shared/copy-to-clipboard/copy-to-clipboard.component.ts
+++ src/app/components/shared/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -1,9 +1,12 @@
-import { Component, ElementRef, HostListener, input, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, input, signal, ViewChild } from '@angular/core';
 import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'button[copy-to-clipboard]',
-  imports: [ClipboardModule],
-  template: `<i #copyIcon [cdkCopyToClipboard]="value() || ''"  class='fi fi-rr-copy-alt flex text-mutedForeground hover:text-accentForeground cursor-pointer transition-colors'>`,
+  imports: [ClipboardModule, MatTooltipModule],
+  template: `<i #tooltip="matTooltip" #copyIcon [matTooltip]="'Copy to Clipboard'" [cdkCopyToClipboard]="value() || ''"
+   class='fi fi-rr-copy-alt flex text-mutedForeground hover:text-accentForeground cursor-pointer transition-colors'
+   (click)="onCopyToClipboardClick(tooltip)">`,
 })
 export class CopyToClipboardComponent {
   value = input<string>();
@@ -14,4 +17,18 @@ export class CopyToClipboardComponent {
     this.copyIcon.nativeElement.click(); // trigger click on <i> element when host is clicked.
     //that will active the [cdkCopyToClipboard] directive
   }
+
+  onCopyToClipboardClick(tooltip: MatTooltip) {
+    tooltip.show()
+    if(tooltip.message !== 'Copied') { // avoid multiple executions of the tooltip handler
+      tooltip.message = 'Copied' // change tooltip message to 'Copied'
+      tooltip.hideDelay = 500;
+  
+      setTimeout(() => { // reset tooltip message to 'Copy' after 2.5 seconds
+        tooltip.message = 'Copy to Clipboard'
+        tooltip.hideDelay = 0;
+      }, 2500);
+    }
+  }
+  
 }
