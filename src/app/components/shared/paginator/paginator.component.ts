599d5524e2dc1d215c9244936522eb38e10a4e71
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-02-09 22:39:29 -0300

    refactor(paginator): update buttons value logic to use signals

--- src/app/components/shared/paginator/paginator.component.ts
+++ src/app/components/shared/paginator/paginator.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, input, output } from '@angular/core';
+import { Component, effect, ElementRef, HostListener, input, output, signal, ViewChild } from '@angular/core';
 // import { fadeInOut, parentAnimations, popUp, slideUpDown } from '../../../animations/transition-animations';
 import { NgxMaskDirective } from 'ngx-mask';
 import { FormsModule } from '@angular/forms';
@@ -17,17 +17,26 @@ export default class PaginatorComponent {
   startsAt = input<number>(1);
   getCurrentPage = output<number>();
 
+  constructor() {
+    effect(() => {
+      this.firstButtonValue = this.getButtonValue(-1);
+      this.secondButtonValue = this.getButtonValue(0);
+      this.thirdButtonValue = this.getButtonValue(1);
+    });
+  }
+  protected firstButtonValue = 0;
+  protected secondButtonValue = 0;
+  protected thirdButtonValue = 0;
 
-  
-  protected currentPage = this.startsAt();
+  protected currentPage = signal<number>(this.startsAt());
   protected secondToLastPage = this.limit() - 1;
   protected showPageSelector = false;
-  protected pageSelectorError = false;
   protected pageSelectorInputValue = '';
+  @ViewChild('pageSelector') pageSelectorInputElementRef!: ElementRef<HTMLInputElement>;
 
   selectPage(page: number) {
-    this.currentPage = page;
-    this.getCurrentPage.emit(this.currentPage);
+    this.currentPage.set(page);
+    this.getCurrentPage.emit(this.currentPage());
   }
 
   onPageSelected(page: number){
@@ -38,44 +47,32 @@ export default class PaginatorComponent {
   }
 
   onPreviousPage(){
-    if(this.currentPage > 1){
-      this.selectPage(this.currentPage - 1);
+    if(this.currentPage() > 1){
+      this.selectPage(this.currentPage() - 1);
     }
   }
 
   onNextPage(){
-    if(this.currentPage < this.limit()){
-      this.selectPage(this.currentPage + 1);
+    if(this.currentPage() < this.limit()){
+      this.selectPage(this.currentPage() + 1);
     }
   }
   
 
   getButtonValue(offset: number): number {
-    if(this.currentPage <= 2) {
+    if(this.currentPage() <= 2) {
       return 2 + offset;
     }
 
-    if(this.limit() === 3 && this.currentPage === 3) { // fix for when there are only 3 pages
+    if(this.limit() === 3 && this.currentPage() === 3) { // fix for when there are only 3 pages
      return  2 + offset;
     }
 
-    if(this.currentPage >= this.limit() - 1) { // if current page is the second to last page or last page
+    if(this.currentPage() >= this.limit() - 1) { // if current page is the second to last page or last page
       return (this.limit() - 2) + offset;
     }
 
-    return this.currentPage + offset;
-  }
-  
-  get firstButtonValue(): number {
-    return this.getButtonValue(-1)
-  }
-  
-  get secondButtonValue(): number {
-    return this.getButtonValue(0);
-  }
-  
-  get thirdButtonValue(): number {
-    return this.getButtonValue(1);
+    return this.currentPage() + offset;
   }
   
 
@@ -85,33 +82,29 @@ export default class PaginatorComponent {
   // Page Selector modal methods
   closePageSelector() {
     this.showPageSelector = false;
-  }
-
-  onClosePageSelector() {
-    this.pageSelectorError = false;
     this.pageSelectorInputValue = '';
-    this.closePageSelector();
   }
 
-  onOpenPageSelector() {
+  openPageSelector() {
     if(this.limit() >= 3) {
       this.showPageSelector = true;
+
+      setTimeout(() => { // timeout to wait for the element to be rendered before interacting with it
+        const inputElement = this.pageSelectorInputElementRef.nativeElement
+        inputElement.focus();
+      }, 100);
     }
   }
 
-  showPageSelectorError() {
-    this.pageSelectorError = true;
-  }
-
-  hideShowPageSelectorError() {
-    this.pageSelectorError = false;
+  togglePageSelector() {
+    this.showPageSelector ? this.closePageSelector() : this.openPageSelector();
   }
 
   @HostListener('window:keydown', ['$event']) // close page selector on esc key press
   handleKeyDown(event: KeyboardEvent): void {
     if (event.key === 'Escape') {
       if (this.showPageSelector) { // close page selector if open
-        this.onClosePageSelector();
+        this.closePageSelector();
       }
     }
   }
@@ -120,11 +113,10 @@ export default class PaginatorComponent {
   onSelectSpecificPage(page: string) {
     const pageNumber = parseInt(page, 10);
     if(pageNumber > this.limit() || pageNumber < 1 || isNaN(pageNumber)) {
-      this.showPageSelectorError();
       return 
     }
     this.selectPage(pageNumber);
-    this.onClosePageSelector();
+    this.closePageSelector();
   }
   
   // prevent user from entering numbers greater than the last page in the input
@@ -139,12 +131,13 @@ export default class PaginatorComponent {
     if(inputValue > this.limit()) {
       this.pageSelectorInputValue = this.limit().toString();
       inputElement.value = this.limit().toString(); // update input value to last page number
+
+    } else if(inputValue < 1) {
+      this.pageSelectorInputValue = '1';
+      inputElement.value = '1'; // update input value to 1
+
     } else {
       this.pageSelectorInputValue = value; // update to allowed value
-
-      if(this.pageSelectorError) {
-        this.hideShowPageSelectorError();
-      }
     }
   }
 
