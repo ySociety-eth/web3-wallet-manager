88ed2b95e836ec3c1a5cf0a3d3f923a78d2021b8
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-02-21 10:09:57 -0300

    feat(default-transitions): add slideAnimation

--- src/app/animations/default-transitions.animations.ts
+++ src/app/animations/default-transitions.animations.ts
@@ -34,6 +34,33 @@ export const popIn = trigger('popIn', [
 //[@popIn]="{ value: '', params: { duration: '200ms', width: 0}}"
 //width:0 will animate width, also for height: 0
 
+
+//================================================= SLIDE =======================================================
+
+export function slideAnimation(triggerName: string, 
+  params: { duration?: string, animateY?:boolean, animateX?:boolean, transform?:string, opacity?:string }) {
+  const animateY = params.animateY ? { height: 0, minHeight: 0, paddingTop: 0, marginTop: 0, paddingBottom: 0, marginBottom: 0 } : { }
+  const animateX = params.animateX ? { width: 0, minWidth: 0, paddingRight: 0, marginRight: 0, paddingLeft: 0, marginLeft: 0 } : { }
+  return trigger(triggerName, [
+    state('void',
+      style({
+        ...animateY,
+        ...animateX,
+        transform: params.transform || 'none',
+        opacity: params.opacity || '0'
+      } as { [key: string]: string | number; }
+      )
+    ),
+    transition(':enter, :leave', animate(params.duration ? `${params.duration} ease-in-out` : '200ms ease-in-out'))
+  ])
+  
+}
+//================================================= END SLIDE =======================================================
+
+
+
+
+
 export const queryAnimations = [
     trigger('queryAnimations', [
       transition(':leave', [
