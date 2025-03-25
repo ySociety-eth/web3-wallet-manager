fcd38ab3f7938b0da192c260c3e98993495ca2ac
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-03-25 17:50:25 -0300

    feat(app): add event listener for successful wallet-connect connection
    Opens register modal when "CONNECT_SUCCESS" is received.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PageLayoutComponent } from "./components/layout/page-layout/page-layout.component";
 import { SidebarSections } from './models/navbar-items.interface';
 import { RouterOutlet } from '@angular/router';
+import { RegisterModalComponent } from './components/dialogs/register-modal/register-modal.component';
+import { RegisterModalService } from './services/register-modal.service';
+import { createQueryAnimations } from './animations/default-transitions.animations';
+import { WalletConnectService } from './services/wallet-connect.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PageLayoutComponent],
+  imports: [RouterOutlet, PageLayoutComponent, RegisterModalComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    createQueryAnimations('queryAnimationsModal', '@popUp, @fadeInOut') // necessary for animate modal when its removed from the DOM
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  protected registerModal = inject(RegisterModalService);
+  private walletConnect = inject(WalletConnectService);
+
   title = 'wallet-manager';
+
+  
+  ngOnInit(): void {
+    this.walletConnect.modal?.subscribeEvents(event => {
+      if (event.data.event == "CONNECT_SUCCESS") {
+        this.registerModal.openModal();
+      }
+    })
+  }
+  
   sidebarSections: SidebarSections[] = [
     {
     label: 'Wallet',
