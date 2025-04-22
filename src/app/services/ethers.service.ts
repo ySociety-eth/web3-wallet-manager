80edcffb4ee94fcbf4930efa59fec53bf4c1d89f
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-04-22 19:49:59 -0300

    fix(ethers.service): use a Eip1193 provider to sign messages

--- src/app/services/ethers.service.ts
+++ src/app/services/ethers.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { WalletConnectService } from "./wallet-connect.service";
 import { BrowserProvider, Eip1193Provider } from "ethers";
-import { BehaviorSubject, filter, from, Observable, Subject, switchMap, take } from "rxjs";
+import { BehaviorSubject, filter, from, Observable, switchMap, take } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -11,10 +11,18 @@ export class EthersService {
     public provider$ = this.providerSubject.asObservable();
 
     constructor(private walletConnectService: WalletConnectService) {
-        var walletConnectProvider: Eip1193Provider;
         walletConnectService.modal?.subscribeProviders( async wcProvider => {
-            walletConnectProvider = wcProvider.eip155 as Eip1193Provider
-            const browserProvider = new BrowserProvider(walletConnectProvider);
+            if (!wcProvider.eip155) {
+                this.providerSubject.next(null); // if no provider, set provider$ to null
+                console.error("No EIP-155 provider found in WalletConnect provider.");
+                return;
+            }
+            const walletConnectProvider = wcProvider.eip155 as Eip1193Provider;
+
+            const eip1193Provider: Eip1193Provider = { // EIP-1193 provider wrapper using WalletConnect provider
+                request: walletConnectProvider.request.bind(walletConnectProvider)
+            }
+            const browserProvider = new BrowserProvider(eip1193Provider);
             this.providerSubject.next(browserProvider); // update the provider$ signal
         })
     }
