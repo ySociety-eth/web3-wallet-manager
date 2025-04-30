983433f9298bdf84e7e058ce963ce27c11bb54fb
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-04-30 19:00:15 -0300

    feat(landing-page): create navbar

--- src/app/components/layout/landing-page/navbar/navbar.component.ts
+++ src/app/components/layout/landing-page/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { AppkitBtnComponent } from "../../../base/appkit-btn/appkit-btn.component";
 
 @Component({
     selector: 'navbar',
-    template: 'navbar',
-    imports: [],
+    host: {
+        class: 'sticky top-0 bg-background/5 backdrop-blur-sm flex items-center justify-between md:px-14 xl:px-16 px-8 py-4 gap-10 border-b border-b-border font-semibold'
+    },
+    templateUrl: './navbar.component.html',
+    imports: [AppkitBtnComponent],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
