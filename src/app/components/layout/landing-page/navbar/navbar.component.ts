5c883f42c5e3acd214b014a02ab97ab8007effeb
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-05-02 23:17:22 -0300

    style(landing-page): justify faq text and reduce opacity from nav

--- src/app/components/layout/landing-page/navbar/navbar.component.ts
+++ src/app/components/layout/landing-page/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 @Component({
     selector: 'navbar',
     host: {
-        class: 'sticky top-0 z-10 bg-background/70 backdrop-blur flex items-center justify-between md:px-14 xl:px-16 px-8 py-4 gap-10 font-semibold'
+        class: 'sticky top-0 z-10 bg-background/55 backdrop-blur flex items-center justify-between md:px-14 xl:px-16 px-8 py-4 gap-10 font-semibold'
     },
     templateUrl: './navbar.component.html',
     imports: [],
